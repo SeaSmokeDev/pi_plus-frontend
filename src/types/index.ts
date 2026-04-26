35c570783dcd25eb3e5967a0e2b65bb46b45ea1f
Ian tauzy <ian.dev@outlook.es>
2026-04-26 23:00:29 +0200

    update types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./common.types";
+export * from "./user.types";
+export * from "./expeditions.types";
+export * from "./warehouse.types";
+export * from "./pallet.types";
+export * from "./box.types";
+export * from "./terminal.types";
