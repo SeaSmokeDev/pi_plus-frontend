3b6ded70abc85b8069a6ea018dceb083ff78e19b
Ian tauzy <ian.dev@outlook.es>
2026-04-27 23:27:33 +0200

    hook expedition terminado
    Para obtener las listas de los gets

---
+++ src/hooks/useExpeditions.ts
@@ -0,0 +1,61 @@
+import type { Expedition } from "../types";
+import { getExpeditionsToday, getExpeditionsByUser, getExpeditionsByAddress } from "../services/expeditionService";
+import { useEffect, useState } from "react";
+
+export function useExpeditions() {
+    const [expeditions, setExpeditions] = useState<Expedition[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    async function loadAll(){
+        try {
+            setLoading(true);
+            setError(null);
+            const data = await getExpeditionsToday();
+            setExpeditions(data);
+        } catch {
+            setError("Error al cargar las expediciones");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    async function searchByAddress(address: string) {
+        try {
+            setLoading(true);
+            setError(null);
+            const data = await getExpeditionsByAddress(address);
+            setExpeditions(data);
+        } catch {
+            setError("Error al buscar expediciones por dirección");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    async function searchByUser(userName: string) {
+        try {
+            setLoading(true);
+            setError(null);
+            const data = await getExpeditionsByUser(userName);
+            setExpeditions(data);
+        } catch {
+            setError("Error al buscar expediciones por usuario");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        loadAll();
+    }, []);
+
+    return {
+        expeditions,
+        loading,
+        error,
+        searchByAddress,
+        searchByUser,
+        reload: loadAll
+    };
+}
