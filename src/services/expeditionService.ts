f1ec333f24ea95dc49cf244debb604d15f544303
Ian tauzy <ian.dev@outlook.es>
2026-04-27 23:26:34 +0200

    expeditionService terminado

--- src/services/expeditionService.ts
+++ src/services/expeditionService.ts
@@ -1,35 +1,42 @@
-import type {
-  Expedition,
-  CreateExpeditionRequest,
-  UpdateExpeditionRequest,
-} from "../types";
-
+import type { Expedition, CreateExpeditionRequest, UpdateExpeditionRequest,} from "../types";
 import { apiRequest } from "./apiClient";
 
-export function getExpeditions() {
-    return apiRequest<Expedition[]>("/expeditions");
+export function getAllExpeditions() {
+    return apiRequest<Expedition[]>("/expediciones");
+}
+
+export function getExpeditionsToday() {
+    return apiRequest<Expedition[]>("/expediciones/today");
 }
 
 export function getExpedition(id: number) {
-    return apiRequest<Expedition>(`/expeditions/${id}`);
+    return apiRequest<Expedition>(`/expediciones/${id}`);
+}
+
+export function getExpeditionsByUser(userName: string) {
+    return apiRequest<Expedition[]>(`/expediciones/nombre/usuario/${userName}`);
+}
+
+export function getExpeditionsByAddress(address: string) {
+    return apiRequest<Expedition[]>(`/expediciones/direccion=${encodeURIComponent(address)}`);
 }
 
 export function createExpedition(expedition: CreateExpeditionRequest) {
-    return apiRequest<Expedition>("/expeditions", {
+    return apiRequest<Expedition>("/expediciones", {
         method: "POST",
         body: JSON.stringify(expedition)
     });
 }
 
 export function updateExpedition(id: number, expedition: UpdateExpeditionRequest) {
-    return apiRequest<Expedition>(`/expeditions/${id}`, {
+    return apiRequest<Expedition>(`/expediciones/${id}`, {
         method: "PUT",
         body: JSON.stringify(expedition)
     });
 }
 
 export function deleteExpedition(id: number) {
-    return apiRequest<void>(`/expeditions/${id}`, {
+    return apiRequest<void>(`/expediciones/${id}`, {
         method: "DELETE"
     });
 }
