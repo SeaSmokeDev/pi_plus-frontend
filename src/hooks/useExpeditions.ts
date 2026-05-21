898e77ca6ef9eb2bf7bd50137f4550af5ed8cffd
Ian tauzy <ian.dev@outlook.es>
2026-05-21 21:26:18 +0200

    eliminando comentarios

--- src/hooks/useExpeditions.ts
+++ src/hooks/useExpeditions.ts
@@ -83,34 +83,6 @@ export function useExpeditions() {
     }
   }
 
-  // async function searchByAddress(address: string) {
-  //   try {
-  //     setLoading(true);
-  //     setError(null);
-  //     const data = await getExpeditionsByAddress(address);
-  //     setExpeditions(data);
-  //   } catch (err) {
-  //     setError("Error al buscar expediciones por direccion");
-  //     console.error("Error searching expeditions by address:", err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-
-  // async function searchByUser(userName: string) {
-  //   try {
-  //     setLoading(true);
-  //     setError(null);
-  //     const data = await getExpeditionsByUser(userName);
-  //     setExpeditions(data);
-  //   } catch (err) {
-  //     setError("Error al buscar expediciones por usuario");
-  //     console.error("Error searching expeditions by user:", err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-
   useEffect(() => {
     void loadAllGroupToday();
   }, []);
